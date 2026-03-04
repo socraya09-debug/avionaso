f184ffc55a8179497d3602d0851fe72be16e1bac
socraya09-debug <socraya09@gmail.com>
2026-03-04 13:29:09 -0400

    yaya

--- destinos.py
+++ destinos.py
@@ -18,3 +18,14 @@ def mostrar_ruta_miami():
     print("Destino: Miami")
     print("Duración: 2 horas 30 minutos")
     print("Precio: USD 350")
+
+    def mostrar_ruta_miami():
+    print("=== RUTA MIAMI ===")
+    print("Origen: Santo Domingo")
+    print("Destino: Miami")
+    print("Duración: 2 horas 30 minutos")
+    print("Precio: USD 350")
+
+
+
+mostrar_ruta_miami()
