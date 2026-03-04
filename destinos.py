a622c2f9b6246aff0d695d79a9e0ceea451b9dd6
socraya09-debug <socraya09@gmail.com>
2026-03-04 13:30:53 -0400

    porfavor

--- destinos.py
+++ destinos.py
@@ -1,25 +1,5 @@
-def mostrar_ruta_miami():
-    print("Ruta: Santo Domingo - Miami")
-
-    def mostrar_ruta_miami():
-    print("=== RUTA MIAMI ===")
-    print("Origen: Santo Domingo")
-    print("Destino: Miami")
 
 def mostrar_ruta_miami():
-    print("=== RUTA MIAMI ===")
-    print("Origen: Santo Domingo")
-    print("Destino: Miami")
-    print("Duración: 2 horas 30 minutos")
-
-    def mostrar_ruta_miami():
-    print("=== RUTA MIAMI ===")
-    print("Origen: Santo Domingo")
-    print("Destino: Miami")
-    print("Duración: 2 horas 30 minutos")
-    print("Precio: USD 350")
-
-    def mostrar_ruta_miami():
     print("=== RUTA MIAMI ===")
     print("Origen: Santo Domingo")
     print("Destino: Miami")
