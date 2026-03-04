00918e5def203b6d8e5a51ac3ba250319f049f8d
socraya09-debug <socraya09@gmail.com>
2026-03-04 13:27:50 -0400

    okaok

--- destinos.py
+++ destinos.py
@@ -1,2 +1,7 @@
 def mostrar_ruta_miami():
     print("Ruta: Santo Domingo - Miami")
+    
+    def mostrar_ruta_miami():
+    print("=== RUTA MIAMI ===")
+    print("Origen: Santo Domingo")
+    print("Destino: Miami")
